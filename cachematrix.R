37a806820d942a46ef379b7b7fa3c8d54eb6cf15
ArKus-kz <67435900+ArKus-kz@users.noreply.github.com>
2020-06-26 14:13:24 +0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-### Following functions cache the inverse of a matrix
+### This functions cache the inverse of a matrix
 ### following function creates a matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
  inv <- NULL
-  set <- function(y){
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(solveMatrix) inv <<- solveMatrix
-  getInverse <- function() inv
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+ set <- function(y){
+ x <<- y
+ inv <<- NULL
+}
+get <- function() x
+setInverse <- function(solveMatrix) inv <<- solveMatrix
+getInverse <- function() inv
+list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-### Following function computes the inverse of the matrix returned by makeCacheMatrix
+### Following function calculates the inverse of the matrix returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  inv <- x$getInverse()
-  if(!is.null(inv)){
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data)
-  x$setInverse(inv)
-  inv   
+inv <- x$getInverse()
+if(!is.null(inv)){
+message("getting cached data")
+return(inv)
+}
+data <- x$get()
+inv <- solve(data)
+x$setInverse(inv)
+inv   
 }
